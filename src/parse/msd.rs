2f394b642b827240f5d26d39982ad2e8281bef2a
Anders429 <anders.stefan.evensen@gmail.com>
2021-10-17 14:58:55 -0700

    msd parsing tests.

--- src/parse/msd.rs
+++ src/parse/msd.rs
@@ -17,7 +17,8 @@
 //! The MSD format is here specified as follows:
 //!
 //! A **parameter** is any string of characters. Note that `#`, `;`, `:`, `\`, and `/` when after
-//! another unescaped `/` must all be escaped with a preceeding `\` character.
+//! another unescaped `/` must all be escaped with a preceeding `\` character. Leading and trailing
+//! whitespace characters are ommitted.
 //!
 //! A **parameter list** is one or more parameters separated by the `:` character and terminated by
 //! the `;` character.
@@ -36,6 +37,7 @@
 /// character or not. Most parameter lists will be tagged, but there are some exceptions, such as,
 /// for example, the separate parameters of the `#BACKGROUND` `dwi` tag. Consumers should only use
 /// the `Untagged` variant if that is what they are explicitly expecting.
+#[derive(Debug, PartialEq)]
 pub(crate) enum ParameterList<'a> {
     /// A tagged parameter list.
     ///
@@ -49,4 +51,291 @@ pub(crate) enum ParameterList<'a> {
 
 pub(crate) fn parse<'a>(input: &'a str) -> impl Iterator<Item = ParameterList<'a>> {
     vec![].into_iter()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{parse, ParameterList};
+
+    #[test]
+    fn empty() {
+        itertools::assert_equal(parse(""), []);
+    }
+
+    #[test]
+    fn comment() {
+        itertools::assert_equal(parse("//foo"), []);
+    }
+
+    #[test]
+    fn tagged_parameter_list_single() {
+        itertools::assert_equal(parse("#foo;"), [ParameterList::Tagged(vec!["foo"])]);
+    }
+
+    #[test]
+    fn tagged_parameter_list_many() {
+        itertools::assert_equal(
+            parse("#foo:bar:baz:qux;"),
+            [ParameterList::Tagged(vec!["foo", "bar", "baz", "qux"])],
+        );
+    }
+
+    #[test]
+    fn tagged_parameter_list_two_lines() {
+        itertools::assert_equal(
+            parse("#foo:\nbar;"),
+            [ParameterList::Tagged(vec!["foo", "bar"])],
+        );
+    }
+
+    #[test]
+    fn tagged_parameter_list_with_comment() {
+        itertools::assert_equal(
+            parse("#foo://comment\nbar;"),
+            [ParameterList::Tagged(vec!["foo", "bar"])],
+        );
+    }
+
+    #[test]
+    fn tagged_parameter_list_escaped_characters() {
+        itertools::assert_equal(
+            parse(r"#\#\;\:/\/;"),
+            [ParameterList::Tagged(vec!["#;://"])],
+        );
+    }
+
+    #[test]
+    fn tagged_parameter_list_elided_semicolon_at_end_of_input() {
+        itertools::assert_equal(parse("#foo"), [ParameterList::Tagged(vec!["foo"])]);
+    }
+
+    #[test]
+    fn tagged_parameter_list_elided_semicolon_with_following_tagged_parameter_list() {
+        itertools::assert_equal(
+            parse("#foo\n#bar;"),
+            [
+                ParameterList::Tagged(vec!["foo"]),
+                ParameterList::Tagged(vec!["bar"]),
+            ],
+        );
+    }
+
+    #[test]
+    fn tagged_parameter_list_unelided_semicolon_without_newline() {
+        itertools::assert_equal(parse("#foo#bar;"), [ParameterList::Tagged(vec!["foo#bar"])]);
+    }
+
+    #[test]
+    fn tagged_parameter_list_elided_semicolon_with_newline_and_space() {
+        itertools::assert_equal(
+            parse("#foo\n #bar;"),
+            [
+                ParameterList::Tagged(vec!["foo"]),
+                ParameterList::Tagged(vec!["bar"]),
+            ],
+        );
+    }
+
+    #[test]
+    fn untagged_parameter_list_single() {
+        itertools::assert_equal(parse("foo;"), [ParameterList::Untagged(vec!["foo"])]);
+    }
+
+    #[test]
+    fn untagged_parameter_list_many() {
+        itertools::assert_equal(
+            parse("foo:bar:baz:qux;"),
+            [ParameterList::Untagged(vec!["foo", "bar", "baz", "qux"])],
+        );
+    }
+
+    #[test]
+    fn untagged_parameter_list_two_lines() {
+        itertools::assert_equal(
+            parse("foo:\nbar;"),
+            [ParameterList::Untagged(vec!["foo", "bar"])],
+        );
+    }
+
+    #[test]
+    fn untagged_parameter_list_with_comment() {
+        itertools::assert_equal(
+            parse("foo://comment\nbar;"),
+            [ParameterList::Untagged(vec!["foo", "bar"])],
+        );
+    }
+
+    #[test]
+    fn untagged_parameter_list_escaped_characters() {
+        itertools::assert_equal(
+            parse(r"\#\;\:/\/;"),
+            [ParameterList::Untagged(vec!["#;://"])],
+        );
+    }
+
+    #[test]
+    fn untagged_parameter_list_elided_semicolon_at_end_of_input() {
+        itertools::assert_equal(parse("foo"), [ParameterList::Untagged(vec!["foo"])]);
+    }
+
+    #[test]
+    fn untagged_parameter_list_elided_semicolon_with_following_tagged_parameter_list() {
+        itertools::assert_equal(
+            parse("foo\n#bar;"),
+            [
+                ParameterList::Untagged(vec!["foo"]),
+                ParameterList::Tagged(vec!["bar"]),
+            ],
+        );
+    }
+
+    #[test]
+    fn untagged_parameter_list_unelided_semicolon_without_newline() {
+        itertools::assert_equal(
+            parse("foo#bar;"),
+            [ParameterList::Untagged(vec!["foo#bar"])],
+        );
+    }
+
+    #[test]
+    fn untagged_parameter_list_unelided_semicolon_with_newline_and_space() {
+        itertools::assert_equal(
+            parse("foo\n #bar;"),
+            [
+                ParameterList::Untagged(vec!["foo"]),
+                ParameterList::Tagged(vec!["bar"]),
+            ],
+        );
+    }
+
+    #[test]
+    fn multiple_tagged_parameter_lists() {
+        itertools::assert_equal(
+            parse("#foo:bar;\n#baz:qux;"),
+            [
+                ParameterList::Tagged(vec!["foo", "bar"]),
+                ParameterList::Tagged(vec!["baz", "qux"]),
+            ],
+        );
+    }
+
+    #[test]
+    fn multiple_untagged_parameter_lists() {
+        itertools::assert_equal(
+            parse("foo:bar;\nbaz:qux;"),
+            [
+                ParameterList::Untagged(vec!["foo", "bar"]),
+                ParameterList::Untagged(vec!["baz", "qux"]),
+            ],
+        );
+    }
+
+    #[test]
+    fn mixed_tagged_and_untagged_parameter_lists() {
+        itertools::assert_equal(
+            parse("#foo:bar;\nbaz:qux;\n#quux:quuz"),
+            [
+                ParameterList::Tagged(vec!["foo", "bar"]),
+                ParameterList::Untagged(vec!["baz", "qux"]),
+                ParameterList::Tagged(vec!["quux", "quuz"]),
+            ],
+        );
+    }
+
+    #[test]
+    fn mixed_tagged_and_untagged_parameter_lists_no_newlines() {
+        itertools::assert_equal(
+            parse("#foo:bar;baz:qux;#quux:quuz"),
+            [
+                ParameterList::Tagged(vec!["foo", "bar"]),
+                ParameterList::Untagged(vec!["baz", "qux"]),
+                ParameterList::Tagged(vec!["quux", "quuz"]),
+            ],
+        );
+    }
+
+    #[test]
+    fn comment_between_parameter_lists() {
+        itertools::assert_equal(
+            parse("#foo:bar;\n//comment\nbaz:qux;"),
+            [
+                ParameterList::Tagged(vec!["foo", "bar"]),
+                ParameterList::Untagged(vec!["baz", "qux"]),
+            ],
+        );
+    }
+
+    #[test]
+    fn tagged_and_untagged_parameter_lists_comments_and_whitespace() {
+        itertools::assert_equal(
+            parse("#foo:bar;\n//comment\n  \n\nbaz:qux;"),
+            [
+                ParameterList::Tagged(vec!["foo", "bar"]),
+                ParameterList::Untagged(vec!["baz", "qux"]),
+            ],
+        );
+    }
+
+    #[test]
+    fn multiple_empty_parameters() {
+        itertools::assert_equal(parse(":"), [ParameterList::Untagged(vec!["", ""])]);
+    }
+
+    #[test]
+    fn only_one_empty_parameter_untagged_elided_semicolon() {
+        itertools::assert_equal(parse(" "), []);
+    }
+
+    #[test]
+    fn only_one_empty_parameter_tagged_elided_semicolon() {
+        itertools::assert_equal(parse("# "), [ParameterList::Tagged(vec![""])]);
+    }
+
+    #[test]
+    fn parameters_strip_leading_whitespace() {
+        itertools::assert_equal(parse("# foo;"), [ParameterList::Tagged(vec!["foo"])]);
+    }
+
+    #[test]
+    fn parameters_strip_trailing_whitespace() {
+        itertools::assert_equal(parse("#foo ;"), [ParameterList::Tagged(vec!["foo"])]);
+    }
+
+    #[test]
+    fn parameters_strip_leading_and_trailing_whitespace() {
+        itertools::assert_equal(parse("# foo ;"), [ParameterList::Tagged(vec!["foo"])]);
+    }
+
+    #[test]
+    fn sequential_empty_tagged_parameter_lists_elided_semicolons() {
+        itertools::assert_equal(
+            parse("#\n#"),
+            [
+                ParameterList::Tagged(vec![""]),
+                ParameterList::Tagged(vec![""]),
+            ],
+        );
+    }
+
+    #[test]
+    fn sequential_empty_tagged_parameter_lists() {
+        itertools::assert_equal(
+            parse("#;#;"),
+            [
+                ParameterList::Tagged(vec![""]),
+                ParameterList::Tagged(vec![""]),
+            ],
+        );
+    }
+
+    #[test]
+    fn sequential_empty_untagged_parameter_lists() {
+        itertools::assert_equal(
+            parse(";;"),
+            [
+                ParameterList::Untagged(vec![""]),
+                ParameterList::Untagged(vec![""]),
+            ],
+        );
+    }
 }
