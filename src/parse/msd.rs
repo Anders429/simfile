92324d7b4edf83f10834b1fa8aac3b1dc59a9e2a
Anders429 <anders.stefan.evensen@gmail.com>
2021-10-16 21:56:25 -0700

    Define parse function for msd.

--- src/parse/msd.rs
+++ src/parse/msd.rs
@@ -4,7 +4,7 @@
 //! *DDR'99* simulator. The format is built on the idea of providing sets of parameters to be
 //! interpreted, in a format like the following:
 //!
-//! ```
+//! ``` msd
 //! #param0:param1:param2;  // Any nonzero number of parameters is acceptable.
 //! ```
 //!
@@ -13,3 +13,40 @@
 //! have allowed for elision of the terminating `;` character. `dwi` specifically has specified
 //! that some parameter lists should not begin with a `#` character. These exceptions are accounted
 //! for within this parser.
+//!
+//! The MSD format is here specified as follows:
+//!
+//! A **parameter** is any string of characters. Note that `#`, `;`, `:`, `\`, and `/` when after
+//! another unescaped `/` must all be escaped with a preceeding `\` character.
+//!
+//! A **parameter list** is one or more parameters separated by the `:` character and terminated by
+//! the `;` character.
+//!
+//! A **tagged parameter list** is a parameter list preceeded by a `#` character.
+//!
+//! A **comment** is any string of characters preceeded by `//` and terminated by the `\n` newline
+//! character.
+//!
+//! An `msd` file is a sequence of parameter lists, tagged parameter lists, comments, and
+//! whitespace in any order.
+
+/// A list of parameters provided together in the simfile.
+///
+/// Parameter lists can either be `Tagged` or `Untagged`, denoting whether they begin with a `#`
+/// character or not. Most parameter lists will be tagged, but there are some exceptions, such as,
+/// for example, the separate parameters of the `#BACKGROUND` `dwi` tag. Consumers should only use
+/// the `Untagged` variant if that is what they are explicitly expecting.
+pub(crate) enum ParameterList<'a> {
+    /// A tagged parameter list.
+    ///
+    /// This is a list of parameters beginning with a `#` character.
+    Tagged(Vec<&'a str>),
+    /// An untagged parameter list.
+    ///
+    /// This is a list of parameters not beginning with a `#` character.
+    Untagged(Vec<&'a str>),
+}
+
+pub(crate) fn parse<'a>(input: &'a str) -> impl Iterator<Item = ParameterList<'a>> {
+    vec![].into_iter()
+}
