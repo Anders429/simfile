b906768ea67b3889cafd24f0296f48c9ebcd7b63
Anders429 <anders.stefan.evensen@gmail.com>
2021-10-20 19:56:45 -0700

    Interpret ARTIST tag in msd files.

--- src/song/interpret/msd.rs
+++ src/song/interpret/msd.rs
@@ -3,6 +3,7 @@ use crate::{internal_log, parse::msd::ParameterList, Song};
 /// Constants for all valid `msd` tags.
 mod tags {
     pub(super) const TITLE: &str = "title";
+    pub(super) const ARTIST: &str = "artist";
 }
 
 pub(crate) fn interpret<I>(parameter_lists: I) -> Result<Song, ()>
@@ -37,6 +38,20 @@ where
                             list.get_unchecked(1)
                         };
                         song.title = Some(title.clone());
+                    }
+                    tags::ARTIST => {
+                        if list.len() != 2 {
+                            internal_log::error!(
+                                "expected 1 parameter for artist, found {}",
+                                list.len() - 1
+                            );
+                            return Err(());
+                        }
+                        let artist = unsafe {
+                            // SAFETY: Just checked that `list` has exactly 2 elements.
+                            list.get_unchecked(1)
+                        };
+                        song.artist = Some(artist.clone());
                     }
                     _ => {
                         internal_log::warn!("skipping unrecognized tag {}", tag);
@@ -115,6 +130,36 @@ mod tests {
     fn title_too_few_parameters() {
         assert_err!(interpret(iter::once(ParameterList::Tagged(vec![
             "TITLE".to_owned(),
+        ]))));
+    }
+
+    #[test]
+    fn artist() {
+        let mut expected = Song::default();
+        expected.artist = Some("foo".to_owned());
+
+        assert_ok_eq!(
+            interpret(iter::once(ParameterList::Tagged(vec![
+                "ARTIST".to_owned(),
+                "foo".to_owned()
+            ]))),
+            expected
+        );
+    }
+
+    #[test]
+    fn artist_too_many_parameters() {
+        assert_err!(interpret(iter::once(ParameterList::Tagged(vec![
+            "ARTIST".to_owned(),
+            "foo".to_owned(),
+            "bar".to_owned(),
+        ]))));
+    }
+
+    #[test]
+    fn artist_too_few_parameters() {
+        assert_err!(interpret(iter::once(ParameterList::Tagged(vec![
+            "ARTIST".to_owned(),
         ]))));
     }
 }
