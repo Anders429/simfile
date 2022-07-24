4b0f1a6ff4e571cfc2b46633f08ddaee4134b761
Anders429 <anders.stefan.evensen@gmail.com>
2022-07-24 16:42:44 -0700

    Formatting.

--- src/song/dwi.rs
+++ src/song/dwi.rs
@@ -1,13 +1,12 @@
 //! Types for representing valid `.dwi` files.
-//! 
+//!
 //! Generic `Song` types can be converted to `.dwi` equivalents, and vice-versa. These `.dwi` types
 //! can be serialized to and deserialized from `.dwi` files directly.
 
 use crate::song;
 
 #[derive(Debug)]
-enum ConversionError {
-}
+enum ConversionError {}
 
 /// Valid states for an individual panel to be in.
 #[derive(Debug, PartialEq)]
